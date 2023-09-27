ffc062874a8c70099cba84267d91f6a67e2ef925
ansman58 <javanslem@gmail.com>
2023-09-27 13:39:51 +0100

    update

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -5,7 +5,11 @@ class UserController {
     try {
       const { name, email, avatar } = req.body;
       const user = await User.create({ name, email, avatar });
-      return res.json(user);
+      return res.status(200).json({
+        status: "success",
+        message: "User created successfully",
+        user,
+      });
     } catch (error) {
       UserController.errorHandler(error, res);
     }
@@ -13,7 +17,7 @@ class UserController {
 
   static async index(req, res) {
     try {
-      const users = await User.findAll({
+      const users = await User.findAndCountAll({
         attributes: ["id", "name", "email", "avatar"],
         include: [
           {
@@ -21,7 +25,70 @@ class UserController {
           },
         ],
       });
-      res.status(200).send(users);
+      res.status(200).send({ status: "success", users });
+    } catch (error) {
+      UserController.errorHandler(error, res);
+    }
+  }
+
+  static async show(req, res) {
+    try {
+      if (!req.params.id) {
+        return res
+          .status(400)
+          .json({ status: "failed", error: "Please provide a valid id" });
+      }
+      const user = await User.findByPk(req.params.id, {
+        attributes: ["id", "name", "email", "avatar"],
+        include: [
+          {
+            model: Events,
+          },
+        ],
+      });
+      return res.status(200).send({ status: "success", user });
+    } catch (error) {
+      UserController.errorHandler(error, res);
+    }
+  }
+
+  static async update(req, res) {
+    try {
+      const { name, email, avatar } = req.body;
+
+      if (!req.params.id) {
+        return res
+          .status(400)
+          .json({ status: "failed", error: "Please provide a valid id" });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      const updated_user = await user.update({ name, email, avatar });
+      return res.status(200).send({
+        status: "success",
+        message: "User updated successfully",
+        updated_user,
+      });
+    } catch (error) {
+      UserController.errorHandler(error, res);
+    }
+  }
+
+  static async destroy(req, res) {
+    try {
+      if (!req.params.id) {
+        return res
+          .status(400)
+          .json({ status: "failed", error: "Please provide a valid id" });
+      }
+      const user = await User.findByPk(req.params.id);
+      const delteUser = await user.destroy();
+      return res.status(200).send({
+        status: "success",
+        message: "User deleted successfully",
+        delteUser,
+      });
     } catch (error) {
       UserController.errorHandler(error, res);
     }
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.post("/", UserController.store);
 router.get("/", UserController.index);
+router.get("/:id", UserController.show);
+router.patch("/:id", UserController.update);
+router.delete("/:id", UserController.destroy);
 
 module.exports = router;
